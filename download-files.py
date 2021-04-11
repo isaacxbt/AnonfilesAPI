e28e9f8fe061ed9510f5f4a882e0468ce589a9d3
Hashed0x0 <70858315+hashed0x0@users.noreply.github.com>
2021-04-11 18:40:26 +0200

    Add files via upload

---
+++ download-files.py
@@ -0,0 +1,10 @@
+import wget
+
+
+try:
+
+    download = wget.download("direct URL") # How? https://encrypting.host/f7nkbiHzgK.gif?key=x05yQJBDd7qgYN
+  
+except Exception as e:
+    print(e)
+    # your file can't be downloaded.
